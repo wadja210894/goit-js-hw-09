7f76d0db8b1cd11a64f539ffa96c9103e99803b2
wadja210894 <54590021+wadja210894@users.noreply.github.com>
2023-03-23 18:39:08 +0000

    Deploying to gh-pages from @ wadja210894/goit-js-hw-09@01ddba4a544f6c4e6c41208f4c860351ce2ab69b 🚀

--- 01-color-switcher.96f09292.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;function d(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(function(){t.style.backgroundColor=d(),o=setInterval((()=>{t.style.backgroundColor=d()}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,n.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.96f09292.js.map
